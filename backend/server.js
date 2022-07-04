8be9f0b39ee5435d0095803bb0eced8b5cd0a2e1
Giriraj Kumawat <Girirajkumawat698@gmail.com>
2022-07-05 02:40:34 +0530

    Signin backend API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import data from './data.js';
-
+import userRouter from './routes/userRoutes.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -20,6 +20,19 @@ mongoose
 
 
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 const port = process.env.PORT || 5000;
